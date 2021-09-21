afe6d3d22ff29c551090122d43946b6501c53a15
Fernando Mota Freitas <fernandom@pop-os.localdomain>
2021-09-21 14:39:40 -0300

    comentando código e removendo partes desnecessárias

--- app.py
+++ app.py
@@ -3,9 +3,10 @@ from flask import Flask
 from flask import jsonify
 from flask import request
 from flask import render_template
-# from livereload import Server    
 app = Flask(__name__)
 
+# Apesar de já possuir um módulo com a definição da classe paciente, precisei criar uma
+# versão para ser usado nesse módulo pois estava enfrentando problemas de importação
 class Patient:
   #Construtor
   def __init__(self):
@@ -14,83 +15,57 @@ class Patient:
     self.oxigenacao = oxigenacao
     self.name = name
     self.gravidade = False
-    # self.name.append(self.first_name)
-    # self.name.append(self.last_name)
     self.name = ''.join((self.first_name," ",self.last_name))
 
 patients = [] # array que vai armazenar os pacientes e seus dados
 
-# @app.route('/', methods=['GET'])
-# def hello_world():
-#     return jsonify({'message' : 'Olá!'})
+# Apenas retorna uma mensagem quando o endpoint padrão é acessado, útil para testar
+# pode ser removido se quiser
+@app.route('/', methods=['GET'])
+def hello_world():
+    return jsonify({'message' : 'Olá!'})
 
-# We're using the new route that allows us to read a date from the URL
+# Retorna a lista de todos os pacientes já registrados quando o endpoint <url_do_app>/pacientes
+# for acessado 
 @app.route('/pacientes', methods = ['GET'])
 def returnAll():
-  # data = request.get_data().decode()
-    # array_resultante = json.loads({patients})
-  # array_resultante = jsonify({'patients' : patients})
-  # return render_template('index.html', array_resultante = array_resultante) 
-  # return jsonify({'patients' : patients})
-  # array_resultante = json.loads(patients)
-  # array_resultante = jsonify({patients})
-  array_resultante = jsonify(patients)
-  return render_template('index.html', array_resultante = array_resultante.json) 
-  # return request.headers.get('your-header-name')
+  array_resultante = jsonify(patients)  # Formata em JSON
+  return render_template('index.html', array_resultante = array_resultante.json) #envia para o template index.html os dados em formato JSON
 
+# Recebe uma requisição POST e exibe no endpoint <url_do_app>/pacientes registrando um paciente 
 @app.route('/pacientes', methods=['POST'])
 def addOne():
-    new_patient = request.get_json()
-    new_patient_data = json.loads(new_patient)
-    patients.append(new_patient_data)
-    print("POST patients", patients)
-    # patients.insert(new_patient)
-    array_resultante = jsonify(patients)
-    # array_resultante = jsonify({patients})
-    return render_template('index.html', array_resultante = array_resultante.json) 
-    # return jsonify({'patients': patients})
+    new_patient = request.get_json() # armazena a requisição em formato JSON
+    new_patient_data = json.loads(new_patient) # recebe os dados da requisição e converte para tipo string
+    patients.append(new_patient_data) # adiciona ao final da array patients instanciada na linha 20
+    print("POST patients", patients) # Apenas para debugar
+    
+    array_resultante = jsonify(patients) # Cria uma nova variável para enviar ao front-end (cliente do médico)
+    return render_template('index.html', array_resultante = array_resultante.json) # envia para o template index.html
 
+# Recebe uma requisição PUT e exibe no endpoint <url_do_app>/pacientes registrando um paciente 
 @app.route('/pacientes', methods=['PUT'])
 def editAll():
-    # patient_stored = Patient() # aqui serao carregados os dados contidos no array já armazenado no server até agora
     
     new_patient = request.get_json()
     new_patient_data = json.loads(new_patient)
+    
     #esse buffer temporario serve para comparar informações dos novos POST request com os que já foram armazenados
     tmp_buffer = []
     tmp_buffer.append(new_patient_data['nome'])
     tmp_buffer.append(new_patient_data['oxigenacao'])
     tmp_buffer.append(new_patient_data['gravidade'])
-    # print("asidoasidoa")
-    print(tmp_buffer)
-    # patient_stored.first_name = new_patient_data
+    print(tmp_buffer) # Apenas para controle, pode retirar se desejar
     for patient in patients:
       # se o nome do paciente já estiver registrado atualiza seu valor de oxigenação e gravidade
       if(patient['nome'] == tmp_buffer[0]):
         patient['oxigenacao'] = tmp_buffer[1]
         patient['gravidade'] = tmp_buffer[2]
-      print ("dentro do put req mas fora do if")
-      print(patient)
-      print("put patients", patients)
+      print ("dentro do put req mas fora do if") # controle (pode retirar se quiser)
+      print(patient)                              # controle
+      print("put patients", patients)              # controle
     array_resultante = jsonify(patients)
     return render_template('index.html', array_resultante = array_resultante.json) 
-    # array_resultante = patients
-    # return jsonify({'patients' : patients})
-    #   if (patie)
-    # for patient in patients
-    #   if patient
-    #     patients[i] = new_patient    
-    # ps = request.get_json()
-    # array_resultante = jsonify({'patients': patients})
-    # array_resultante = patients
-    # print(array_resultante)
-    # return render_template('index.html', array_resultante = array_resultante) 
-    # array_resultante = json.loads(patients)
-    # return render_template('index.html', array_resultante = array_resultante)     
-    # array_resultante=array_resultante
 
 if __name__ == '__main__':
     app.run(debug=True, port=3000)
-    # pyautogui.hotkey('f5')
-    # server = Server(app.wsgi_app)
-    # server.serve(port=3000)
